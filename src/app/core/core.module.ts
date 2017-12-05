da3f4a9be5d60377854a3d6284c37081e0175e45
zhouH0901 <zhezhou@my.bridgeport.edu>
2017-12-05 01:47:02 -0500

    add basic layout page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SkipSelf,Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent
+  ],
+  exports:[
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent
+  ]
 })
-export class CoreModule { }
+export class CoreModule { 
+
+  constructor(@Optional() @SkipSelf() parent:CoreModule){
+    if(parent){
+      throw new Error('the core module is extating, not be load')
+    }
+  }
+
+}
