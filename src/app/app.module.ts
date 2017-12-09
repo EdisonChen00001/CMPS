68141643ce39f77c3d9bcac289c7da5e11426d87
timH0901 <zho.dev@gmail.com>
2017-12-08 23:07:31 -0500

    install dependences for material cdk/Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {CoreModule} from './core/core.module'; 
-
+import { CoreModule } from './core/core.module'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppComponent } from './app.component';
 
 
@@ -11,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
     CoreModule
   ],
   providers: [],
